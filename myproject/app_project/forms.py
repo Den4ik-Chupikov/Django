6b5eff8496e61c4c5f41c302d8b3ac39ddc8646c
den4ik-chupikov <denis-chupikov@ukr.net>
2023-06-27 16:37:44 +0300

    forms inner

---
+++ myproject/app_project/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+class MyForm(forms.Form):
+    nickname = forms.CharField(label='My nickname', max_length = 100)
+    age = forms.IntegerField(label='My age')
+    
+class LoginForm(forms.Form):
+    user_name = forms.CharField(label = "User name", max_length = 30)
+    password = forms.CharField(label = "Password", max_length = 20)
+    email = forms.EmailField(label ="Email", max_length = 30)
+           
